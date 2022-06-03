f3060af0ff54dee965588e23d940fd71b996a906
PamelaPajarillo <pamela.pajarillo@rutgers.edu>
2022-06-03 17:09:19 -0400

    Working prototype of interactive README, checking to see if it works on Github

--- make_md.py
+++ make_md.py
@@ -1,200 +1,101 @@
-import os
+from utils import *
 
-import requests
+# Output Markdown File
+out_md = open("README.md","w")
 
-update_journal = False
+# Title
+out_md.write("#  **A Living Review of Quantum Information Science in Particle Physics**\n\n")
 
-myfile = open("HEPQIS.tex")
-myfile_out = open("README.md","w")
+# Abstract
+out_md.write("*Inspired by \"A Living Review of Machine Learning for Particle Physics\", the goal of this repository is to provide an extensive list of citations for those developing and applying quantum information approaches to experimental, phenomenological, or theoretical analyses.  Applications of quantum information science to high energy physics is a relatively new field of research.  This repository will be updated as often as possible with the relevant literature.  Suggestions are most welcome.*\n\n")
 
-myfile_out.write("#  **A Living Review of Quantum Information Science in Particle Physics**\n\n")
+# Intro
+out_md.write("The goal of this repository is to collect references for quantum information science as applied to particle and nuclear physics.  \n\n")
 
-myfile_out.write("*Inspired by \"A Living Review of Machine Learning for Particle Physics\", the goal of this repository is to provide an extensive list of citations for those developing and applying quantum information approaches to experimental, phenomenological, or theoretical analyses.  This repository will be updated as often as possible.  Suggestions are most welcome.*\n\n")
+# Input Files
+csv_file = 'HEPQIS.csv'
+bib_file = 'HEPQIS.bib'
+txt_file = 'CATEGORIES.txt'
 
-# myfile_out.write("[![download](https://img.shields.io/badge/download-review-blue.svg)](https://iml-wg.github.io/HEPML-LivingReview/review/hepml-review.pdf)\n\n")
+# Read CSV and convert to dataframe
+csv_entries = pd.read_csv(csv_file)
+df_csv = pd.DataFrame(csv_entries)
 
-myfile_out.write("The goal of this repository is to collect references for quantum information science as applied to particle and nuclear physics.  \n\n")
+# Read BibTeX and convert to dataframe
+with open(bib_file) as bibtex_file:
+    bib_database = bibtexparser.load(bibtex_file)
 
-###This bit is slightly modified from Kyle Cranmer https://github.com/cranmer/inspire_play
-def summarize_record(recid):
-    url = 'https://labs.inspirehep.net/api/arxiv/'+str(recid)
-    max_authors = 5
-    r = requests.get(url)
-    mini_dict = {'recid':recid}
-    if 'metadata' in r.json():
-        data = r.json()['metadata']
-        mini_dict.update({'title':data['titles'][0]['title']})
-        if len(data['authors'])>max_authors:
-            #mini_dict.update({'authors':[a['full_name'] for a in data['authors'][:max_authors]]+['et. al.']})
-            mini_dict.update({'authors':"; ".join([a['full_name'] for a in data['authors'][:max_authors]]+['et. al.'])})
-        else:
-            mini_dict.update({'authors':[a['full_name'] for a in data['authors']]})
+df_bib = pd.DataFrame(bib_database.entries)
+df_bib['title'] = df_bib['title'].str.strip('{}')
+df = df_csv.merge(df_bib, on='ID')
 
-        if 'collaborations' in data:
-            mini_dict.update({'collaboration': data['collaborations'][0]['value']})
+def fetch_eprint_url(entry_str,check):
+    # Look at eprint
+    if not check:
+        return 'https://arxiv.org/abs/' + str(entry_str)
 
-        mini_dict.update({'arxiv_eprint': data['arxiv_eprints'][0]['value']})
-        mini_dict.update({'url': 'https://arxiv.org/abs/'+data['arxiv_eprints'][0]['value']})
-        mini_dict.update({'creation_date': data['legacy_creation_date']})
+def fetch_doi_url(entry_str,check):
+    # Look at eprint
+    if not check:
+        return 'https://doi.org/'+ str(entry_str)
 
-        if 'journal_title' in data:
-            mini_dict.update({'journal_title':data['publication_info'][0]['journal_title']})
-        if 'journal_volume' in data:
-            mini_dict.update({'journal_volume':data['publication_info'][0]['journal_volume']})
-        if 'page_start' in data:
-            mini_dict.update({'page_start':data['publication_info'][0]['page_start']})
-        if 'journal_year' in data:
-            mini_dict.update({'journal_year':data['publication_info'][0]['year']})
+# Check and Make URLs 
+df["eprint_check"] = df["eprint"].isnull()
+df["doi_check"] = df["doi"].isnull()
+df["eprint_url"] = df.apply(lambda x: fetch_eprint_url(x.eprint, x.eprint_check), axis = 1)
+df["doi_url"] = df.apply(lambda x: fetch_doi_url(x.doi, x.doi_check), axis = 1)
 
-        if 'dois' in data:
-            mini_dict.update({'doi': data['dois'][0]['value']})
-    return mini_dict
+# Compress dataframe with useful information
+df = df[["ID", "title", "Categories", "Description", "eprint_url", "doi_url"]]
 
-def convert_from_bib(myline):
-    #Not the most elegant way, but quick and dirty.  Files are not big, so this doesn't take long.
+# Parse CATEGORIES.txt
+categories = []
+with open(txt_file) as cat_file:
+    categories = cat_file.read().splitlines() 
+            
+# Get Categories and Subcategories
+for entry in categories:
+    # Parse main category and subcategory
+    maincategory = entry.split(", ")[0]
+    subcategories = entry.split(", ")[1:]    
 
-    myline = myline.replace(" ","").replace("\n","")
+    # Print Title of Main Category
+    out_md.write("##  **%s**\n\n" % maincategory.strip('\''))
 
-    myfile_bib = open("HEPQIS.bib")
-    mylines = []
-    for line in myfile_bib:
-        mylines+=[line]
-        pass
-    myentry = []
-    for i in range(len(mylines)):
-        if myline in mylines[i]:
-            j = i+1
-            myentry+=[mylines[i]]
-            while "@" not in mylines[j]:
-                myentry+=[mylines[j]]
-                j+=1
-                pass
-            pass
-        pass
-    myentry_dict = {}
-    for entry in myentry:
-        entry_cleaned = entry.replace("\"{","").replace("}\",","").replace("},","")
-        entry_cleaned = entry_cleaned.replace(" =","=")
-        entry_cleaned = entry_cleaned.replace("= ","=")
-        first_entry = entry_cleaned.split("=")[0]
-        if "title" in first_entry and not "booktitle" in first_entry:
-            myentry_dict["title"] = entry_cleaned.split("title")[1].split("=")[1].split("\n")[0]
-            pass
-        elif "eprint" in first_entry:
-            myentry_dict["eprint"] = entry_cleaned.split("eprint")[1].split("=")[1].split("\n")[0].replace("\"","").replace(",","").replace("\'","").replace(" ","")
-            if "{" in myentry_dict["eprint"]:
-                myentry_dict["eprint"] = myentry_dict["eprint"][1:]
-                pass
-        elif "doi" in first_entry:
-            myentry_dict["doi"] = entry_cleaned.split("doi")[1].split("=")[1].split("\n")[0].replace("\"","").replace(",","").replace("\'","").replace(" ","")
-        elif "url" in first_entry:
-            myentry_dict["url"] = entry_cleaned.split("url")[1].split("=")[1].split("\n")[0].replace("\"","").replace(",","").replace("\'","").replace(" ","")
-        else:
-            #print(entry_cleaned)
-            pass
-        pass
+    # Retrieve papers by checking for substring in categories
+    df_cat = df.loc[df['Categories'].str.contains(maincategory, case=False)]
+    cat_papers = df_cat.values.tolist()
 
-    if "eprint" in myentry_dict and 'doi' not in myentry_dict and update_journal:
-        #check inspire
-        inspire_dict = summarize_record(myentry_dict['eprint'])
-        if 'doi' in inspire_dict:
-            print("Updating journal ref for ",myline)
-            myentry_dict['doi'] = inspire_dict['doi']
+    # Formatting and write to file
+    for paper in cat_papers:
+        out_md.write("<details>\n")
+        if (paper[4] is not None) and (paper[5] is not None):
+            out_md.write("<summary> <a href=\"%s\"> %s</a> [<a href=\"%s\">DOI</a>]</summary>" % (paper[4], paper[1], paper[5]))
+        elif (paper[4] is not None):
+            out_md.write("<summary> <a href=\"%s\"> %s</a></summary>" % (paper[4], paper[1]))
+        elif (paper[5] is not None):
+            out_md.write("<summary> <a href=\"%s\"> %s</a></summary>" % (paper[5], paper[1]))
 
-            #print(inspire_dict)
+        out_md.write("%s\n" % paper[3].strip('\"'))
+        out_md.write("</details>\n")
+    
+    out_md.write("\n\n")
+    for subentry in subcategories:
+        out_md.write("###  **%s**\n\n" % subentry.strip('\''))
 
-            myfile_bib_copy = open("HEPQIS_copy.bib","w")
-            myfile_bib = open("HEPQIS.bib")
-            for line in myfile_bib:
-                myfile_bib_copy.write(line)
-                if myentry_dict['eprint'] in line and "eprint" in line:
-                    if "journal_title" in inspire_dict:
-                        myfile_bib_copy.write("      journal=\""+inspire_dict['journal_title']+"\",\n")
-                    if "journal_volume" in inspire_dict:
-                        myfile_bib_copy.write("      volume=\""+inspire_dict['journal_volume']+"\",\n")
-                    if "page_start" in inspire_dict:
-                        myfile_bib_copy.write("      pages=\""+inspire_dict['page_start']+"\",\n")
-                    if "doi" in inspire_dict:
-                        myfile_bib_copy.write("      doi=\""+inspire_dict['doi']+"\",\n")
-                        pass
-                    pass
-                pass
-            os.system("mv HEPQIS_copy.bib HEPQIS.bib")
-            #exit(1)
+        # Retrieve papers by checking for substring in categories
+        df_subcat = df.loc[df['Categories'].str.contains(subentry, case=False)]
+        cat_papers = df_subcat.values.tolist()
+        # Formatting and write to file
+        for paper in cat_papers:
+            out_md.write("<details>\n")
+            if (paper[4] is not None) and (paper[5] is not None):
+                out_md.write("<summary> <a href=\"%s\"> %s</a> [<a href=\"%s\">DOI</a>]</summary>" % (paper[4], paper[1], paper[5]))
+            elif (paper[4] is not None):
+                out_md.write("<summary> <a href=\"%s\"> %s</a></summary>" % (paper[4], paper[1]))
+            elif (paper[5] is not None):
+                out_md.write("<summary> <a href=\"%s\"> %s</a></summary>" % (paper[5], paper[1]))
 
-    if "title" not in myentry_dict:
-        print(myline)
-        print(myentry)
-        print("We are in trouble ! ")
-    if "eprint" in myentry_dict:
-        paper=""
-        if "doi" in myentry_dict:
-            paper=f" [[DOI](https://doi.org/{myentry_dict['doi']})]"
-        elif "url" in myentry_dict:
-            paper=f" [[url]({myentry_dict['url']})]"
-        return "["+myentry_dict["title"]+"](https://arxiv.org/abs/"+myentry_dict["eprint"]+")"+paper
-    elif "doi" in myentry_dict:
-        return "["+myentry_dict["title"]+"](https://doi.org/"+myentry_dict["doi"]+")"
-    elif "url" in myentry_dict:
-        return "["+myentry_dict["title"]+"]("+myentry_dict["url"]+")"
-    else:
-        return myentry_dict["title"]
-    return myline
-
-itemize_counter = 0
-for line in myfile:
-
-    if "author" in line:
-        continue
-
-    if "\\item \\textbf{" in line:
-        line = line[0:line.find("}")]+line[line.find("}")+1:-1]
-    line = line.replace("\\textbf{","")
-
-    if "textit{" in line:
-        continue
-
-    if "item" in line:
-        if "begin{itemize}" in line:
-            itemize_counter+=1
-        elif "end{itemize}" in line:
-            itemize_counter-=1
-        else:
-            #print(itemize_counter,line)
-            if (itemize_counter==1):
-                hascites = len(line.split("cite"))
-                if (hascites==1):
-                    if "Experimental" not in line:
-                        myfile_out.write("* "+line.replace(r"\item","")+"\n")
-                    else:
-                        myfile_out.write("*  Experimental results. *This section is incomplete as there are many results that directly and indirectly (e.g. via flavor tagging) use modern machine learning techniques.  We will try to highlight experimental results that use deep learning in a critical way for the final analysis sensitivity.*\n\n")
-                else:
-                    myfile_out.write("* "+line.replace(r"\item","").split(r"~\cite")[0]+".\n\n")
-                    mycites = line.split(r"~\cite{")[1].split("}")[0].split(",")
-                    for cite in mycites:
-                        myfile_out.write("    * "+convert_from_bib(cite)+"\n")
-                        pass
-                    myfile_out.write("\n")
-                    pass
-                pass
-            elif "cite" in line:
-                mybuffer = ""
-                for j in range(itemize_counter-1):
-                     mybuffer+="    "
-                     pass
-                if (":~" in line):
-                    myfile_out.write(mybuffer+"* "+line.split(r"~\cite{")[0].split(r"\item")[1]+"\n\n")
-                    mycites = line.split(r"~\cite{")[1].replace("}","").split(",")
-                    for cite in mycites:
-                        myfile_out.write(mybuffer+"    * "+convert_from_bib(cite)+"\n")
-                        pass
-                    myfile_out.write("\n")
-                else:
-                    myfile_out.write(mybuffer+"* "+line.split(r"~\cite{")[0].split(r"\item")[1]+"\n\n")
-                    mycites = line.split(r"~\cite{")[1].split("}")[0].split(",")
-                    for cite in mycites:
-                        myfile_out.write(mybuffer+"    * "+convert_from_bib(cite)+"\n")
-                        pass
-                    myfile_out.write("\n")
-                    pass
-                pass
+            out_md.write("%s\n" % paper[3].strip('\"'))
+            out_md.write("</details>\n")
+        out_md.write("\n")
